ef5c48c5e4017cb8ca0242820025a230d8cb6264
ChrisDelvalle90 <christian_mskt@hotmail.com>
2022-07-19 15:22:46 -0400

    Criacao da lista de compras

---
+++ js/script.js
@@ -0,0 +1,55 @@
+
+let listaCompras = []
+
+function adicionarItem()
+{
+    let item = document.getElementById("item").value
+    //listaCompras[listaCompras.length] = item
+    if(item == "")
+    {
+        alert("Campo item não preenchido!")
+    }
+    else
+    {
+        if(listaCompras.indexOf(item) < 0)
+        {
+            listaCompras.push(item)
+            imprimir()
+        }
+        else
+        {
+            alert("Ítem já adicionado!")
+        }
+        limpar()
+    }
+}
+
+function removerItem()
+{
+    let itemRemover = document.getElementById("itemRemover").value
+    let indiceRemover = listaCompras.indexOf(itemRemover)
+
+    if(indiceRemover < 0)
+    {
+        alert("Item não encontrado!")
+    }
+    else 
+    {
+        listaCompras.splice(indiceRemover, 1)
+        alert("Item removido com sucesso!")
+        imprimir()
+    }
+    limpar()
+}
+
+function limpar()
+{
+    document.getElementById("item").value = ""
+    document.getElementById("itemRemover").value = ""
+}
+
+function imprimir()
+{
+    document.getElementById("lista").innerText = listaCompras.join(" | ")
+}
+
